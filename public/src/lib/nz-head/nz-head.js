9495964f9c9fefbc5ee4037f6c74be51ef500f5e
Niza Toshpulatov <niza@hey.com>
2023-09-19 07:54:47 +0200

    Migrate lib to separate repo

--- public/src/lib/nz-head/nz-head.js
+++
@@ -1,8 +0,0 @@
-/** @type {CustomElementDefinition<"nz-head">} */
-export const definition = {
-  name: "nz-head",
-  moduleUrl: import.meta.url,
-  init({ slots }) {
-    document.head.append(...slots.default);
-  },
-};
--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,8 @@
+import { bootstrap } from "https://cdn.jsdelivr.net/gh/nizamiza/nz@main/lib/core/bootstrap.js";
 import { definition as socialLink } from "./components/nz-social-link/nz-social-link.js";
 import { definition as footer } from "./components/nz-footer/nz-footer.js";
 import { definition as layout } from "./layouts/nz-layout/nz-layout.js";
 import { definition as homePage } from "./pages/nz-home/nz-home.js";
-import { bootstrap } from "./lib/core/bootstrap.js";
 import baseStyles from "../css/base.css" assert { type: "css" };
 import resetStyles from "../css/reset.css" assert { type: "css" };
 
