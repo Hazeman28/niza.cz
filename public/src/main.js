213e8c5d43fa48039c813955e73416bd1ebd6e55
Niza Toshpulatov <niza@hey.com>
2023-10-14 07:28:20 +0200

    Update stuff

--- public/src/main.js
+++ public/src/main.js
@@ -1,12 +1,8 @@
 import { bootstrap } from "https://cdn.jsdelivr.net/gh/nizamiza/nz@main/lib/core/bootstrap.js";
 import { definition as socialLink } from "./components/nz-social-link/nz-social-link.js";
 import { definition as footer } from "./components/nz-footer/nz-footer.js";
+import { definition as gravatar } from "./components/nz-gravatar/nz-gravatar.js";
 import { definition as layout } from "./layouts/nz-layout/nz-layout.js";
 import { definition as homePage } from "./pages/nz-home/nz-home.js";
-import baseStyles from "../css/base.css" assert { type: "css" };
-import resetStyles from "../css/reset.css" assert { type: "css" };
 
-await bootstrap(
-  [socialLink, footer, layout, homePage],
-  [baseStyles, resetStyles],
-);
+await bootstrap([socialLink, footer, layout, homePage, gravatar]);
